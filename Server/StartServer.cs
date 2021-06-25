adeb0b7d0b47a341af6e5a3bd890d3ce1edeb354
Tomas L <xlizakt@stuba.sk>
2021-06-25 17:19:36 +0200

    Socket create 02

--- Server/StartServer.cs
+++ Server/StartServer.cs
@@ -7,31 +7,59 @@ namespace Server
 {
     class StartServer
     {
-        private Socket listener;
+        private Socket socket, handler;
         private IPHostEntry host;
         private IPAddress ipAddress;
-        IPEndPoint localEndPoint;
+        private IPEndPoint localEndPoint;
 
         public StartServer()
         {
             Set();
             Create();
+            Bind();
+            Listen();
+            Accept();
+            Clean();
+            
         }
 
         void Set()
         {
             host = Dns.GetHostEntry("127.0.0.1");
             ipAddress = host.AddressList[0];
-            localEndPoint = new IPEndPoint(ipAddress, 11000);
+            localEndPoint = new IPEndPoint(ipAddress, 7777);
         }
         void Create()
         {
-            listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            if (listener == null)
+            socket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            if (socket == null)
             {
                 Console.WriteLine("Socket creation error");
             }
             Console.WriteLine("Socket created");
+        }
+        void Bind()
+        {
+            socket.Bind(localEndPoint);
+           Console.WriteLine("Socket bound");
+        }
+        void Listen()
+        {
+            socket.Listen(10);
+            Console.WriteLine("Socket listening");
+        }
+        void Accept()
+        {
+            Console.WriteLine("Waiting for connection");
+            handler = socket.Accept();
+            Console.WriteLine("Conected");
+        }
+        void Clean()
+        {
+            socket.Shutdown(SocketShutdown.Receive);
+            socket.Close();
+            handler.Shutdown(SocketShutdown.Receive);
+            handler.Close();
         }
     }
 }
